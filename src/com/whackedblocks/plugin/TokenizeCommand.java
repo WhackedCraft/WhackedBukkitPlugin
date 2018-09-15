3be573240016667c04c7c6f870f7522278c137ce
Jonbi2 <marcellobardus8@gmail.com>
2018-09-15 20:48:52 +0200

    Tokenizing diamonds works

--- src/com/whackedblocks/plugin/TokenizeCommand.java
+++ src/com/whackedblocks/plugin/TokenizeCommand.java
@@ -1,45 +1,17 @@
 package com.whackedblocks.plugin;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.web3j.crypto.Credentials;
-import org.web3j.crypto.ECKeyPair;
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.methods.response.Web3ClientVersion;
-import org.web3j.protocol.http.HttpService;
-
-import com.whackedblocks.plugin.Exchange;
-
-import java.io.IOException;
-import java.math.BigInteger;
+import org.bukkit.inventory.ItemStack;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
 
 public class TokenizeCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-
-        Web3j web3 = Web3j.build(new HttpService(WhackedPlugin.instance.getConfig().getString("web3provider")));
-
-        BigInteger publicKey = new BigInteger(WhackedPlugin.instance.getConfig().getString("publicKey"), 16);
-        BigInteger privateKey = new BigInteger(WhackedPlugin.instance.getConfig().getString("privateKey"), 16);
-        ECKeyPair ecKeyPair = new ECKeyPair(privateKey, publicKey);
-        Credentials credentials = Credentials.create(ecKeyPair);
-
-        BigInteger gasPrice = new BigInteger(WhackedPlugin.instance.getConfig().getString("gasPrice"));
-        BigInteger gasLimit = new BigInteger(WhackedPlugin.instance.getConfig().getString("gasLimit"));
-
-        String contractAddress = WhackedPlugin.instance.getConfig().getString("contractAddress");
-
-        try {
-            Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().send();
-            WhackedPlugin.logger.info(web3ClientVersion.getWeb3ClientVersion());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Exchange exchangeContract = Exchange.load(contractAddress, web3, credentials, gasPrice, gasLimit);
 
         if(!(commandSender instanceof Player)) {
             WhackedPlugin.instance.getLogger().info("Console can't use command " + s);
@@ -48,6 +20,44 @@ public class TokenizeCommand implements CommandExecutor {
         }
         Player player = (Player)commandSender;
 
-        return false;
+        if(strings.length != 2) {
+            return false;
+        }
+
+        String address = strings[0];
+        int count = Integer.valueOf(strings[1]);
+
+        if(count <= 0) {
+            commandSender.sendMessage("Liczba przedmiotow do tokenizacji nie moze byc mniejsza/rowna 0.");
+            return true;
+        }
+
+        Exchange contract = WhackedPlugin.instance.getContract();
+
+        ItemStack is = player.getInventory().getItemInMainHand();
+
+        if(is.getType() == Material.AIR) {
+            commandSender.sendMessage("Nie trzymasz przedmiotu w glownej rece.");
+            return true;
+        }
+
+        if(is.getAmount() < count) {
+            commandSender.sendMessage("Liczba przedmiotow do tokenizacji nie moze byc mniejsza/rowna 0.");
+            return true;
+        }
+
+        try {
+            TransactionReceipt receipt = contract.assign(address, is.getType().name() + "," + Integer.toString(count)).send();
+            is.setAmount(is.getAmount() - count);
+            String txId = receipt.getTransactionHash();
+            String message = "Twoje " + count + " " + is.getType().name() + " zostaly zamienone w token!";
+            commandSender.sendMessage(message);
+            commandSender.sendMessage("txid: " + txId);
+            player.sendTitle(count + " " + is.getType().name(), "zostaly zamienione w token!", 10, 40, 10);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
     }
 }
--- src/com/whackedblocks/plugin/WhackedPlugin.java
+++ src/com/whackedblocks/plugin/WhackedPlugin.java
@@ -2,26 +2,74 @@ package com.whackedblocks.plugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.web3j.crypto.CipherException;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
 
+import java.io.IOException;
+import java.math.BigInteger;
 import java.util.logging.Logger;
 
 public class WhackedPlugin extends JavaPlugin {
 
     static WhackedPlugin instance;
     static Logger logger;
+    private Web3j web3j;
+    private Exchange contract;
 
     @Override
     public void onEnable() {
         instance = this;
         logger = this.getLogger();
         saveDefaultConfig();
-        Bukkit.getLogger().info("Super plugin wlaczony!");
+        setupWeb3();
         getCommand("tokenize").setExecutor(new TokenizeCommand());
     }
 
     @Override
     public void onDisable() {
         Bukkit.getLogger().info("Super plugin wylaczony!");
+    }
+
+    private void setupWeb3() {
+        String provider = WhackedPlugin.instance.getConfig().getString("web3provider");
+        web3j = Web3j.build(new HttpService(provider));
+
+        Credentials credentials = null;
+        try {
+            credentials = WalletUtils.loadCredentials("notarealpassword",
+                    WhackedPlugin.instance.getDataFolder().getAbsolutePath().concat("/wallet.json"));
+        } catch (IOException | CipherException e) {
+            e.printStackTrace();
+        }
+
+        BigInteger gasPrice = new BigInteger(WhackedPlugin.instance.getConfig().getString("gasPrice"));
+        BigInteger gasLimit = new BigInteger(WhackedPlugin.instance.getConfig().getString("gasLimit"));
+
+        String contractAddress = WhackedPlugin.instance.getConfig().getString("contractAddress");
+
+        try {
+            Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
+            WhackedPlugin.logger.info(web3ClientVersion.getWeb3ClientVersion());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        WhackedPlugin.instance.getLogger().info("Provider: " + provider);
+        WhackedPlugin.instance.getLogger().info("Contract address: " + contractAddress);
+        WhackedPlugin.instance.getLogger().info("My address: " + credentials.getAddress());
+
+        WhackedPlugin.instance.getLogger().info("Gas price: " + gasPrice.toString());
+        WhackedPlugin.instance.getLogger().info("Gas limit: " + gasLimit.toString());
+
+        this.contract = Exchange.load(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    public Exchange getContract() {
+        return contract;
     }
 
 }
